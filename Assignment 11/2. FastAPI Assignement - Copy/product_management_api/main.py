8fc45727e9cb41eb5b514c4670c63c8649e6f161
AkshitaInnogent <akshita.jaiswal@innogent.in>
2025-10-31 15:11:11 +0530

    Update main.py

--- Assignment 11/2. FastAPI Assignement - Copy/product_management_api/main.py
+++ Assignment 11/2. FastAPI Assignement - Copy/product_management_api/main.py
@@ -6,18 +6,15 @@ app = FastAPI(title="Product Management API")
 
 @app.on_event("startup")
 async def startup_event():
-    print("🚀 Starting Product Management API...")
     await prisma.connect()
-    print("✅ Connected to Prisma database")
 
 @app.on_event("shutdown")
 async def shutdown_event():
     await prisma.disconnect()
-    print("🔌 Disconnected from Prisma")
 
 # Include routers
 app.include_router(companies.router)
 app.include_router(categories.router)
 app.include_router(products.router)
 
-print("✅ Routers included successfully.")
+
